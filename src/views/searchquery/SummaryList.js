6c5e4b0ea0300428dc34f222668cfcf9bcef6388
Zero Qu <zhidongqu@gmail.com>
2020-03-08 01:11:12 -0800

    fix no record bug

--- src/views/searchquery/SummaryList.js
+++ src/views/searchquery/SummaryList.js
@@ -3,14 +3,18 @@ import SummaryItem from './SummaryItem';
 
 export default function SummaryList(props) {
   const resultRecords = props.resultRecords;
-  const renderedResultRecords = resultRecords.map((resultRecord) => {
-    return <SummaryItem key={resultRecord["record"]["recordNum"]} score={resultRecord["score"]} record={resultRecord["record"]} />
-  });
-  return (
-    <div>
-      <ol className="list-group">
-        {renderedResultRecords}
-      </ol>
-    </div>
-  );
+  if (resultRecords) {
+    const renderedResultRecords = resultRecords.map((resultRecord) => {
+      return <SummaryItem key={resultRecord["record"]["recordNum"]} score={resultRecord["score"]} record={resultRecord["record"]} />
+    });
+    return (
+      <div>
+        <ol className="list-group">
+          {renderedResultRecords}
+        </ol>
+      </div>
+    );
+  } else {
+    return (<div> </div>);
+  }
 }
